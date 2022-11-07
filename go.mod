ff91a1dadad94d17e2a0f998317b799580247890
ishuah <kariuki@ishuah.com>
2022-11-07 13:52:49 +0300

    02 Connecting to the pseudoterminal

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module germx
 
 go 1.19
 
-require fyne.io/fyne/v2 v2.2.3
+require (
+	fyne.io/fyne/v2 v2.2.3
+	github.com/creack/pty v1.1.18
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20220621085403-9a2652634e93 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"github.com/creack/pty"
 )
 
 func main() {
@@ -14,6 +19,25 @@ func main() {
 	ui := widget.NewTextGrid()       // Create a new TextGrid
 	ui.SetText("I'm on a terminal!") // Set text to display
 
+	c := exec.Command("/bin/bash")
+	p, err := pty.Start(c)
+
+	if err != nil {
+		fyne.LogError("Failed to open pty", err)
+		os.Exit(1)
+	}
+
+	defer c.Process.Kill()
+
+	p.Write([]byte("ls\r"))
+	time.Sleep(1 * time.Second)
+	b := make([]byte, 1024)
+	_, err = p.Read(b)
+	if err != nil {
+		fyne.LogError("Failed to read pty", err)
+	}
+	// s := fmt.Sprintf("read bytes from pty.\nContent:%s",  string(b))
+	ui.SetText(string(b))
 	// Create a new container with a wrapped layout
 	// set the layout width to 420, height to 200
 	w.SetContent(
